80e2656fc05c56413d214f4b7746f01eab2d9438
supremearchitect12 <johnmaliadeng@gmail.com>
2025-05-24 09:27:26 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Cursor tracking
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+// Pulse awakening
+const pulse = document.querySelector('.pulse');
+setTimeout(() => {
+  pulse.style.opacity = '1';
+}, 7300); // 7.3 seconds
 
